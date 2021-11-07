f58fc0838c734b40eed576ea70f52d92fbd663a0
Aman Kumar <amankumar.k@commerceiq.ai>
2021-11-07 15:56:05 +0530

    RateLimiter: Added Rate limiting to Submission and User COntroller

--- src/main/java/com/projects/onlinejudge/controller/UserController.java
+++ src/main/java/com/projects/onlinejudge/controller/UserController.java
@@ -2,6 +2,7 @@ package com.projects.onlinejudge.controller;
 
 import com.projects.onlinejudge.dto.UserDTO;
 import com.projects.onlinejudge.service.impl.UserServiceImpl;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,13 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
 
+    private static final String ORDER_SERVICE ="orderService" ;
+
     @Autowired
     private UserServiceImpl userService;
 
     @PostMapping("/register")
+    @RateLimiter(name=ORDER_SERVICE, fallbackMethod = "rateLimiterFallback")
     public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
         boolean success = userService.addUser(userDTO);
         return new ResponseEntity<>(success? "User registered successfully":
                 "Unable to register user", HttpStatus.OK);
     }
+
+    public ResponseEntity<String> rateLimiterFallback(Exception e) {
+        return new ResponseEntity<String>("User Controller does not permit further calls", HttpStatus.TOO_MANY_REQUESTS);
+    }
+
 }
